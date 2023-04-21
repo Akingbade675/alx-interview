5879b15a3efc5be435e6a7d048f066c7818af7b1
Akingbade675 <akingbadebasit26@gmail.com>
2023-04-21 01:58:35 +0000

    solved the pascal triangle problem

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''A module that determines the pascal triangle of a number'''
+
+
+def pascal_triangle(n):
+    '''
+    Returns a list of lists of integers representing the
+    Pascal’s triangle of n.
+    '''
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(1, n + 1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                row.append(1)
+                continue
+
+        row.append(triangle[i - 2][j - 1] + triangle[i - 2][j])
+
+        triangle.append(row)
+
+    return triangle
