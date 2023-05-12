3521546c0f045bc348c62a2d6b8064c2efb12623
Akingbade675 <akingbadebasit26@gmail.com>
2023-05-12 00:06:22 +0000

    fixed pycodestyle errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,8 @@
 '''
 A script that reads stdin line by line and computes metrics:
   Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-  <status code> <file size> (if the format is not this one, the line must be skipped)
+  <status code> <file size> (if the format is not this one, the line
+  must be skipped)
 '''
 
 
@@ -20,26 +21,27 @@ total_size = 0
 count = 0
 try:
 
-  for line in sys.stdin:
-    parser = line.split()
-    
+    for line in sys.stdin:
+        parser = line.split()
+
     # skip the line if the format doesn't match above
     if len(parser) != 9:
-      continue
+        continue
 
     file_size = int(parser[-1])
     status_code = parser[-2]
 
     total_size += file_size
     count += 1
-    
+
     # checks if the status_code is exists in the dict `status_codes`
     if status_code in status_codes.keys():
-      status_codes[status_code] += 1
+        status_codes[status_code] += 1
 
     if count == 10:
-      count = 0
-      print('File size: {}'.format(total_size))
-      [print('{} {}'.format(code, count)) for code, count in sorted(status_codes.items()) if count != 0]
+        count = 0
+        print('File size: {}'.format(total_size))
+        [print('{} {}'.format(code, count))
+            for code, count in sorted(status_codes.items()) if count != 0]
 except Exception:
-  pass
+    pass
