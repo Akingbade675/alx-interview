f546e046a2e49d2db573a91933e4269fe7511c92
Akingbade675 <akingbadebasit26@gmail.com>
2023-07-03 23:56:10 +0000

    fixed error in island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-'''Island Perimeter - ALX Interview'''
+"""Island Perimeter - ALX Interview"""
 
 
 def check_val(x):
-    '''_summary_
+    """_summary_
 
     Args:
         n (_type_): _description_
 
     Returns:
         _type_: _description_
-    '''
+    """
     if (x == 0):
         return 1
     return 0
 
 
 def island_perimeter(grid):
-    '''_summary_
+    """_summary_
 
     Args:
         grid (_type_): _description_
-    '''
+    """
 
     row = len(grid)
     col = len(grid[0])
@@ -31,6 +31,7 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(col):
             assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
                 if i-1 < 0:
                     x += 1
